2726e27226a603c4fa1bc2b102faa07f710ccd43
fedex955 <federico.crucilla95@gmail.com>
2025-10-15 11:37:18 +0200

    added a read_users to main.py

--- main.py
+++ main.py
@@ -46,12 +46,3 @@ def read_user(user_id: int):
         if user["id"] == user_id:
             return user
     raise HTTPException(status_code=404, detail="Utente non trovato")
-
-# DELETE - Cancella un utente
-@app.delete("/users/{user_id}")
-def delete_user(user_id: int):
-    for i, user in enumerate(users_db):
-        if user["id"] == user_id:
-            deleted_user = users_db.pop(i)
-            return {"message": "Utente eliminato", "user": deleted_user}
-    raise HTTPException(status_code=404, detail="Utente non trovato")
